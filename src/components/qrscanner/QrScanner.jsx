18f16f98f2df208b51d39b35ee409ce226007331
nicolaszc <nzakrzewicz@gmail.com>
2024-11-27 02:20:53 -0300

    result

--- src/components/qrscanner/QrScanner.jsx
+++ src/components/qrscanner/QrScanner.jsx
@@ -6,13 +6,23 @@ import Wrapper from '@/layouts/Wrapper';
 
 function QrScanner() {
 
+    const handleScan = (result) => {
+        if (result?.[0]?.rawValue) {
+          console.log(result[0].rawValue);
+          window.open(result[0].rawValue, "_blank", "noreferrer");
+        } else if (result?.rawValue) {
+          console.log(result.rawValue);
+          window.open(result.rawValue, "_blank", "noreferrer");
+        } else {
+          console.log("No data found");
+        }
+      }
     return (
         <>
             <Wrapper>
                 <Row>
                     <Col className='mt-5'>
-                        <Scanner onScan={(result) => console.log(result)} viewFinderBorder={18}
-          containerStyle={{ borderRadius: 16 }}/>
+                        <Scanner onScan={(result) => handleScan(result)} />
                     </Col>                   
                 </Row>   
             </Wrapper> 
